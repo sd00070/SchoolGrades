1b582f2b26880877f6cf32d4e0c880ac8d085f47
Spencer Dent <sd00070@my.westga.edu>
2021-10-18 02:21:27 -0400

    create ConcreteGradingStrategies
    (need to implement getValue in CompositeGrade to test)

---
+++ src/test/java/edu/westga/cs/schoolgrades/model/MeanGradingStrategy.java
@@ -0,0 +1,21 @@
+package edu.westga.cs.schoolgrades.model;
+
+import java.util.List;
+
+/**
+ * Used to calculate the average (mean) of the list of grades (reducing the list
+ * to a single value).
+ * 
+ * @author Spencer Dent
+ * @version 2021-10-18
+ */
+public class MeanGradingStrategy implements GradingStrategy {
+
+	@Override
+	public double totalGrades(List<Grade> grades) {
+		return grades
+				.stream()
+				.map((grade) -> grade.getValue())
+				.reduce(0.0, Double::sum) / (double) grades.size();
+	}
+}
