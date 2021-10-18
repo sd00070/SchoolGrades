f3ab035c640a65cbc3d92bc5b9d9d4c9f525ebf7
Spencer Dent <sd00070@my.westga.edu>
2021-10-18 05:27:20 -0400

    create abstract GradingStrategyDecorator class
    (will test with concrete implementation)

---
+++ src/main/java/edu/westga/cs/schoolgrades/model/GradingStrategyDecorator.java
@@ -0,0 +1,36 @@
+package edu.westga.cs.schoolgrades.model;
+
+import java.util.List;
+
+/**
+ * Serves as the basis for all GradingStrategyDecorators. Provides the most
+ * basic functionality common between GradingStrategyDecorator classes.
+ * 
+ * @author Spencer Dent
+ * @version 2021-10-18
+ */
+public abstract class GradingStrategyDecorator implements GradingStrategy {
+
+	private GradingStrategy gradingStrategy;
+
+	/**
+	 * Constructor basis used by all GradingStrategyDecorator classes.
+	 * 
+	 * @param decoratedGradingStrategy - the GradingStrategy to be decorated
+	 * @precondition decoratedGradingStrategy != null
+	 * @throws IllegalArgumentException if decoratedGradingStrategy == null
+	 */
+	public GradingStrategyDecorator(GradingStrategy decoratedGradingStrategy) {
+		if (decoratedGradingStrategy == null) {
+			throw new IllegalArgumentException("GradingStrategy to be decorated cannot be null");
+		}
+
+		this.gradingStrategy = decoratedGradingStrategy;
+	}
+
+	@Override
+	public double totalGrades(List<Grade> grades) {
+		return this.gradingStrategy.totalGrades(grades);
+	}
+
+}
