d4cf3cbf93595979255b882c3ff2c32534f0eb55
Spencer Dent <sd00070@my.westga.edu>
2021-11-29 17:57:28 -0500

    replace dependency w/ mock & implement mocked method

--- src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeGetValue.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeGetValue.java
@@ -1,6 +1,8 @@
 package edu.westga.cs.schoolgrades.model;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,28 +10,29 @@ public class TestWeightedGradeGetValue {
 
 	private static final double DELTA = 0.001;
 	private WeightedGrade weightedGrade;
-	private SimpleGrade simpleGrade;
-	
+	private Grade mockGrade;
+
 	@BeforeEach
 	public void setup() {
-		simpleGrade = new SimpleGrade(100);
+		mockGrade = mock(Grade.class);
+		when(mockGrade.getValue()).thenReturn(100.00);
 	}
-	
+
 	@Test
 	public void shouldApplyZeroWeight() {
-		weightedGrade = new WeightedGrade(simpleGrade, 0);
+		weightedGrade = new WeightedGrade(mockGrade, 0);
 		assertEquals(0, weightedGrade.getValue(), DELTA);
 	}
-	
+
 	@Test
 	public void shouldApplyWeightOfOne() {
-		weightedGrade = new WeightedGrade(simpleGrade, 1);
+		weightedGrade = new WeightedGrade(mockGrade, 1);
 		assertEquals(100, weightedGrade.getValue(), DELTA);
 	}
-	
+
 	@Test
 	public void shouldApplyWeightBetweenZeroAndOne() {
-		weightedGrade = new WeightedGrade(simpleGrade, 0.5);
+		weightedGrade = new WeightedGrade(mockGrade, 0.5);
 		assertEquals(50, weightedGrade.getValue(), DELTA);
 	}
 
