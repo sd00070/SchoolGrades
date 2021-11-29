017eb865ab895c150956901cc6da37bfb06f2c8b
Spencer Dent <sd00070@my.westga.edu>
2021-11-29 18:10:16 -0500

    remove dependency via mocking

--- src/test/java/edu/westga/cs/schoolgrades/model/TestCompositeGradeConstructor.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestCompositeGradeConstructor.java
@@ -1,6 +1,8 @@
 package edu.westga.cs.schoolgrades.model;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.Test;
 
 public class TestCompositeGradeConstructor {
@@ -16,7 +18,7 @@ public class TestCompositeGradeConstructor {
 	
 	@Test
 	public void shouldHaveNoGradesWhenCreated() {
-		CompositeGrade grade = new CompositeGrade(new SumOfGradesStrategy());
+		CompositeGrade grade = new CompositeGrade(mock(GradeCalculationStrategy.class));
 		assertTrue(grade.getGrades().isEmpty());
 	}
 
