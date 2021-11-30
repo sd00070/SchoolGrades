1476b086bb36c5ad7af9236a91307711170d3585
Spencer Dent <sd00070@my.westga.edu>
2021-11-29 23:24:05 -0500

    remove dependency via mocking

--- src/test/java/edu/westga/cs/schoolgrades/model/TestSumOfGradesStrategyCalculate.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestSumOfGradesStrategyCalculate.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -22,9 +25,14 @@ public class TestSumOfGradesStrategyCalculate {
 	
 	@BeforeEach
 	public void setup() {
-		grade0 = new SimpleGrade(10);
-		grade1 = new SimpleGrade(20);
-		grade2 = new SimpleGrade(30);
+		grade0 = mock(Grade.class);
+		when(grade0.getValue()).thenReturn(10.0);
+		
+		grade1 = mock(Grade.class);
+		when(grade1.getValue()).thenReturn(20.0);
+		
+		grade2 = mock(Grade.class);
+		when(grade2.getValue()).thenReturn(30.0);
 		
 		grades = new ArrayList<Grade>();
 		
